507784a913efb2f3b47ed979c9b0732a6efb7efc
kaan-sirin <111517323+kaan-sirin@users.noreply.github.com>
2023-12-30 00:21:46 +0100

    added pandas cheatsheet

--- personlig/src/app/cheatsheets/page.tsx
+++ personlig/src/app/cheatsheets/page.tsx
@@ -18,5 +18,8 @@ export default function Page() {
         <Link style={{color: 'black', textDecoration:'none' }} href="/"> &larr; Back</Link>
         </div>
         <h1>Cheatsheets</h1>
+        <ul>
+            <li><Link href="https://colab.research.google.com/drive/1PAOwA-SZrhPndyksl8Um32z0ghBwtKG9?usp=sharing">Pandas</Link></li>
+        </ul>
     </div >)
 }
